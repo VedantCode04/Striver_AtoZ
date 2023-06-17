395e4ab30fe23429810b7975f7ee105292312799
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-06-17 19:17:32 +0530

    Create move 0s to end(lc 283).cpp

---
+++ arrays/easy/move 0s to end(lc 283).cpp
@@ -0,0 +1,20 @@
+//leetcode 283
+
+class Solution {
+ public:
+  void moveZeroes(vector<int>& arr) {
+      int size = arr.size();
+      int j = 0;
+      for(int i = 0; i < size; ++i){
+          if(arr[i] != 0){
+              swap(arr[i], arr[j]);
+              j++;
+          }
+      }
+
+      while(j < size){
+          arr[j] = 0;
+          j++;
+      }
+  }
+};
