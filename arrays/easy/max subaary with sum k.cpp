4ef93235e29b8f955738ab0047be06fdf0cb8529
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-06-22 20:32:54 +0530

    Create max subaary with sum k.cpp

---
+++ arrays/easy/max subaary with sum k.cpp
@@ -0,0 +1,25 @@
+//https://practice.geeksforgeeks.org/problems/longest-sub-array-with-sum-k0809/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=longest-sub-array-with-sum-k
+
+int lenOfLongSubarr(int a[], int n, int k)
+{
+    unordered_map<long long, int> preSum;
+    int sum = 0, maxLen = 0;
+    long long rem = 0;
+    for (int i = 0; i < n; i++) {
+        sum = sum + a[i];
+
+        if (sum == k) {
+            maxLen = i + 1;
+        }
+        rem = sum - k;
+        if (preSum.find(rem) != preSum.end()) {
+            maxLen = max(maxLen, i - preSum[rem]);
+        }
+
+        if (preSum.find(rem) == preSum.end()) {
+            preSum[rem] = i;
+        }
+    }
+
+    return maxLen;
+};
