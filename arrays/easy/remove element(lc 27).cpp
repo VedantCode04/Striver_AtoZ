a1b5bbe4d4767e149a52683a1d9f7b7625f13fc4
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-06-29 21:05:35 +0530

    Create remove element(lc 27).cpp

---
+++ arrays/easy/remove element(lc 27).cpp
@@ -0,0 +1,18 @@
+//leetcode 27
+//TC: o(N)
+//SC: O(1)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j = 0;
+        for(int i = 0; i < nums.size() ; i++){
+            if(nums[i] != val){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+
+        return j;
+    }
+};
