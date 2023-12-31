fc83447da0ba8361df03278c3e43fb9605a0978b
Vedant <91217411+VedantCode04@users.noreply.github.com>
2023-12-31 20:32:27 +0530

    Create lower bound.cpp

---
+++ binary search/1d/lower bound.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <typeinfo>
+#define vec(x) vector<x>
+#define veci vector<int>
+#define vveci vector<vector<int>>
+#define push(x) push_back(x)
+#define set(x) set<x>
+#define uset(x) unordered_set<x>
+#define map(x, y) map<x, y>
+#define umap(x, y) unordered_map<x, y>
+
+using namespace std;
+
+template <typename T>
+
+void print(T& a)
+{
+    for (auto val : a) {
+        cout << val << " ";
+    }
+}
+
+void sol(veci& a, int n)
+{
+	int x;
+	cin >> x;
+	int ind = n-1;
+
+	int low = 0, high = n-1;
+
+	while(low <= high){
+		int mid = low + (high - low)/2;
+
+		if(a[mid] < x){
+			low = mid + 1;
+		} else {
+			ind = mid;
+			high = mid - 1;
+		}
+	}
+
+	cout << "lower position = " << ind << endl;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    veci a;
+
+    for (int j = 0; j < n; j++) {
+        int x;
+        cin >> x;
+        a.push(x);
+    }
+
+    sol(a, n);
+}
